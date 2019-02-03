898f5a7561095a0153e18906f688ed03a01bcdb0
why <18080690877@163.com>
2019-02-03 17:55:04 +0800

    第一次使用git提交代码....

---
+++ test_git/src/com/why/testgit/TestGit.java
@@ -0,0 +1,12 @@
+package com.why.testgit;
+
+/**
+ * @author WangHongyun
+ * @time 2019年2月3日 下午5:53:52
+ * @description
+ */
+public class TestGit {
+	public static void main(String[] args) {
+		System.out.println("第一次使用git！！！");
+	}
+}
